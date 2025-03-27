71dcc3bf727ff6729c3560b15e6f08cf4f90d2bb
biog4m3 <biog4m3ryt@gmail.com>
2025-03-27 13:30:20 -0300

    Add authentication routes and middleware for user registration and token verification

--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const db = require('./db');
 
-app.get('/', (req, res) => {
-
-    db.default.$connect();
-    db.default.event.findMany().then((events) => {
-        res.send(events);
-    }).catch((error) => {
-        console.log(error);
-        res.send('Error');
-    }).finally(() => {
-        db.default.$disconnect();
-    });
-});
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ src/routers/auth_route.js
@@ -0,0 +1,8 @@
+const route = require('express').Router();
+const prisma = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+route = route.post('/register', async (req, res) => {
+    
+});
